77c463d25bda5e3a8d453081d817c8938d335fe9
Kgrah <kylelgrah@gmail.com>
2018-08-19 16:57:54 -0700

    first couple structs

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/kgrah/MonkeyLang/token"
+
 //The Lexer type
 type Lexer struct {
 	input        string
@@ -11,5 +13,39 @@ type Lexer struct {
 //NewLexer ...Constructor
 func NewLexer(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = token.NewToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = NewToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = NewToken(token.LPAREN, l.ch)
+	case ')':
+		tok = NewToken(token.RPAREN, l.ch)
+	case '{':
+		tok = NewToken(token.LBRACE, l.ch)
+	case '}':
+		tok = NewToken(token.RBRACE, l.ch)
+	case ',':
+		tok = NewToken(token.COMMA, l.ch)
+	case '+':
+		toke = NewToken(token.PLUS, l.ch)
+	}
 }
--- token/token.go
+++ token/token.go
@@ -7,6 +7,12 @@ type Token struct {
 	Literal string
 }
 
+//The book has this function in the lexer.go file
+//NewToken ...Constructor
+func NewToken(tokenType TokenType, ch byte) Token {
+	return Token{Type: tokenType, Literal: string(ch)}
+}
+
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
